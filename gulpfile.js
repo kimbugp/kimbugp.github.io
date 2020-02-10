f245396a0b8d53307d655e064efd9dc73234629b
Kimbugwe Simon Peter <kimbsimon2@gmail.com>
2020-02-10 14:21:29 +0300

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,9 @@ const merge = require("merge-stream");
 const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
+const imagemin = require('gulp-imagemin')
 const uglify = require("gulp-babel-minify");
+let changed = require('gulp-changed');
 
 
 // Load package.json for banner
@@ -45,36 +47,38 @@ function browserSyncReload(done) {
 
 // Clean vendor
 function clean() {
-    return del(["./vendor/"]);
+    return del(["./build/vendor/"]);
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
+    let dst = './build/vendor/'
     // Bootstrap
-    var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-        .pipe(gulp.dest('./vendor/bootstrap'));
+    let bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+        .pipe(gulp.dest(`${dst}bootstrap`));
     // Font Awesome CSS
-    var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-        .pipe(gulp.dest('./vendor/fontawesome-free/css'));
+    let fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
+        .pipe(gulp.dest(`${dst}fontawesome-free/css`));
     // Font Awesome Webfonts
-    var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-        .pipe(gulp.dest('./vendor/fontawesome-free/webfonts'));
+    let fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
+        .pipe(gulp.dest(`${dst}fontawesome-free/webfonts`));
     // jQuery Easing
-    var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-        .pipe(gulp.dest('./vendor/jquery-easing'));
+    let jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+        .pipe(gulp.dest(`${dst}jquery-easing`));
     // jQuery
-    var jquery = gulp.src([
+    let jquery = gulp.src([
         './node_modules/jquery/dist/*',
         '!./node_modules/jquery/dist/core.js'
     ])
-        .pipe(gulp.dest('./vendor/jquery'));
+        .pipe(gulp.dest(`${dst}jquery`));
     return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jquery, jqueryEasing);
 }
 
 // CSS task
 function css() {
+    let cssSrc = "./src/scss/**/*.scss", cssDst = 'build/css';
     return gulp
-        .src("./src/scss/**/*.scss")
+        .src(cssSrc)
         .pipe(plumber())
         .pipe(sass({
             outputStyle: "expanded",
@@ -87,22 +91,22 @@ function css() {
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest(cssDst))
         .pipe(rename({
             suffix: ".min"
         }))
         .pipe(cleanCSS())
-        .pipe(gulp.dest("./css"))
+        .pipe(gulp.dest(cssDst))
         .pipe(browsersync.stream());
 }
 
 
 // JS task
 function js() {
+    let jsSrc = './src/js/*.js', jsDst = 'build/js';
     return gulp
         .src([
-            './src//js/*.js',
-            '!./src/js/*.min.js'
+            jsSrc
         ])
         .pipe(uglify())
         .pipe(header(banner, {
@@ -111,7 +115,17 @@ function js() {
         .pipe(rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest('./js'))
+        .pipe(gulp.dest(jsDst))
+        .pipe(browsersync.stream());
+}
+
+//image task
+function imgSquash() {
+    let imgSrc = 'src/images/*.+(png|jpg|gif)', imgDst = 'build/images';
+    return gulp.src(imgSrc)
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst))
         .pipe(browsersync.stream());
 }
 
@@ -124,12 +138,13 @@ function watchFiles() {
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, gulp.parallel(css, js, imgSquash));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
+exports.image = imgSquash
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
